752109b0e512ffcf7eee88676cf15ea6a2c18b1e
bdkim13 <byoungdo.kim@gmail.com>
2021-06-10 07:36:58 -0700

    add mpi-world.c

---
+++ examples/mpi-world.c
@@ -0,0 +1,19 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int 
+    //  Initialize MPI environment
+
+    // Decide the number of processes
+    
+    // Rank of the process
+
+
+    // printf() displays the string inside quatation
+
+    // Finalize MPI environment
+
+    return 0;
+}
+
